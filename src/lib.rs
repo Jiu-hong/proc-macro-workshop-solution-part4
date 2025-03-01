e5e3dd0056bb01b28e4c1d489dc19e167733a222
Jiu-hong <jiu.hong.sun@gmail.com>
2025-03-01 22:55:08 +0800

    03ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use proc_macro::TokenStream;
+
 use proc_macro2::Span;
 use quote::ToTokens;
 use syn::Result;
@@ -16,6 +17,30 @@ pub fn sorted(_: TokenStream, input: TokenStream) -> TokenStream {
 pub(crate) fn expand(input_item: Item) -> Result<proc_macro2::TokenStream> {
     // eprintln!("input is {:#?}", input_item);
     if let Item::Enum(s) = input_item {
+        let variants = &s.variants;
+        let slice = &mut variants.iter().collect::<Vec<_>>()[..];
+        for (index, _) in variants.iter().enumerate() {
+            let (left, right) = slice.split_at_mut(index);
+            let compared_object = left.last();
+            if compared_object.is_none() {
+                continue;
+            }
+            for x in right {
+                if x.ident <= compared_object.unwrap().ident {
+                    println!("something wrong");
+                    return Err(syn::Error::new(
+                        x.ident.span(),
+                        // Span::call_site(),
+                        format!(
+                            "{} should sort before {}",
+                            x.ident,
+                            compared_object.unwrap().ident
+                        ),
+                    ));
+                }
+            }
+        }
+
         Ok(s.to_token_stream())
     } else {
         return Err(syn::Error::new(
